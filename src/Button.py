9256ea5719190ffee93e1a8af2ab8c95ca05f055
OkilSaber <okil-saber.lakhdari@epitech.eu>
2022-04-24 03:53:32 +0900

    Add draw function and fix the on_click argument type

--- src/Button.py
+++ src/Button.py
@@ -1,14 +1,28 @@
+import pygame
+
+
 class Button:
     def __init__(self,
                  position: tuple[int, int],
                  size: tuple[int, int],
                  color: tuple[int, int, int],
                  text: str,
-                 on_click: function,
+                 on_click,
                  ):
         self.position = position
         self.size = size
         self.color = color
         self.text = text
-        self.on_click = function
+        self.on_click = on_click
         return
+
+    def draw(self, screen: pygame.Surface):
+        pygame.draw.rect(screen,
+                         self.color,
+                         [
+                             self.position[0],
+                             self.position[1],
+                             self.size[0],
+                             self.size[1]
+                         ],
+                         )
