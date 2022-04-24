343369dee44dcc970a4c64b6b0f92bf79ce0999a
OkilSaber <okil-saber.lakhdari@epitech.eu>
2022-04-24 19:21:30 +0900

    Update button class to add text rendering

--- src/Button.py
+++ src/Button.py
@@ -1,28 +1,48 @@
+from typing import Tuple
 import pygame
 
 
 class Button:
-    def __init__(self,
-                 position: tuple[int, int],
-                 size: tuple[int, int],
-                 color: tuple[int, int, int],
-                 text: str,
-                 on_click,
-                 ):
+    def __init__(
+        self,
+        position: Tuple[int, int],
+        size: Tuple[int, int],
+        color: Tuple[int, int, int],
+        hover_color: Tuple[int, int, int],
+        text_color: Tuple[int, int, int],
+        text: str,
+        font: str,
+        text_size: int,
+        text_position: Tuple[int, int],
+        on_click,
+    ):
         self.position = position
         self.size = size
         self.color = color
+        self.hover_color = hover_color
+        self.text_color = text_color
         self.text = text
         self.on_click = on_click
+        self.font = font
+        self.text_size = text_size
+        self.pygame_font = pygame.font.SysFont(font, text_size)
+        self.pygame_text = self.pygame_font.render(
+            self.text,
+            True,
+            self.text_color
+        )
+        self.text_position = text_position
         return
 
     def draw(self, screen: pygame.Surface):
-        pygame.draw.rect(screen,
-                         self.color,
-                         [
-                             self.position[0],
-                             self.position[1],
-                             self.size[0],
-                             self.size[1]
-                         ],
-                         )
+        pygame.draw.rect(
+            screen,
+            self.color,
+            [
+                self.position[0],
+                self.position[1],
+                self.size[0],
+                self.size[1]
+            ],
+        )
+        screen.blit(self.pygame_text, self.text_position)
