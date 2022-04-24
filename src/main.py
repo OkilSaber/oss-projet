cea43cba7ae4f1141f3414761e39971a2930ea42
OkilSaber <okil-saber.lakhdari@epitech.eu>
2022-04-24 18:17:24 +0900

    Update main loop to display the background

--- src/main.py
+++ src/main.py
@@ -1,26 +1,19 @@
 import pygame
-import sys
-import colors
+from Game import Game
 
 pygame.init()
-res = (1280, 720)
-screen = pygame.display.set_mode(res)
-width = screen.get_width()
-height = screen.get_height()
-smallfont = pygame.font.SysFont('Corbel', 35)
-text = smallfont.render('quit', True, colors.white)
-while True:
+game = Game()
+bg = pygame.image.load("assets/menu_background.png")
+while game.running:
+    game.screen.blit(bg, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
+            pygame.QUIT
+            game.running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40:
-                pygame.quit()
-    screen.fill((60, 25, 60))
-    mouse = pygame.mouse.get_pos()
-    if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40:
-        pygame.draw.rect(screen, colors.light, [width/2, height/2, 140, 40])
-    else:
-        pygame.draw.rect(screen, colors.dark, [width/2, height/2, 140, 40])
-    screen.blit(text, (width/2+50, height/2))
+            mouse = pygame.mouse.get_pos()
+            print(mouse)
+            game.check_buttons_click(mouse)
+    for button in game.buttons:
+        button.draw(screen=game.screen)
     pygame.display.update()
