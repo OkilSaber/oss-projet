a09ffa157a28c60f0b6800fd5d47f43e32a92fef
OkilSaber <okil-saber.lakhdari@epitech.eu>
2022-04-24 21:28:55 +0900

    Add constants

---
+++ src/constants/assets.py
@@ -0,0 +1,6 @@
+button_click_sound = "assets/button_click.mp3"
+background_music = "assets/background_music.ogg"
+eat_sound = "assets/eat_sound.ogg"
+die_sound = "assets/die_sound.ogg"
+
+menu_background_image = "assets/menu_background.png"
