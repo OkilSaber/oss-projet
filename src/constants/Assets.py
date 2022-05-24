584237551ed5a8dabebe5b560fe10cc13a63769f
OkilSaber <okil-saber.lakhdari@epitech.eu>
2022-05-24 21:03:09 +0900

    Add second player assets

--- src/constants/Assets.py
+++ src/constants/Assets.py
@@ -25,3 +25,22 @@ class Assets():
     tail_up = "assets/tail_up.png"
     tail_left = "assets/tail_left.png"
     tail_right = "assets/tail_right.png"
+
+    # corner body
+    body_bottomleft_second = "assets/body_bottomleft_second.png"
+    body_bottomright_second = "assets/body_bottomright_second.png"
+    body_topleft_second = "assets/body_topleft_second.png"
+    body_topright_second = "assets/body_topright_second.png"
+    # line body
+    body_horizontal_second = "assets/body_horizontal_second.png"
+    body_vertical_second = "assets/body_vertical_second.png"
+    head_down_second = "assets/head_down_second.png"
+    # head
+    head_up_second = "assets/head_up_second.png"
+    head_left_second = "assets/head_left_second.png"
+    head_right_second = "assets/head_right_second.png"
+    # tail
+    tail_down_second = "assets/tail_down_second.png"
+    tail_up_second = "assets/tail_up_second.png"
+    tail_left_second = "assets/tail_left_second.png"
+    tail_right_second = "assets/tail_right_second.png"
